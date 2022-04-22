2d335c34c26abd9da320e18810fa9a0f21dade2e
Masaya Sato <sato.masaya.dev@gmail.com>
2022-04-22 18:32:44 +0900

    Add several course assignments

---
+++ sqlds/week3/quize6.sql
@@ -0,0 +1,8 @@
+-- All of the questions in this quiz refer to the open source Chinook Database.
+-- Please familiarize yourself with the ER diagram in order to familiarize
+-- yourself with the table and column names in order to write accurate queries
+-- and get the appropriate answers.
+
+-- Use a UNION to create a list of all the employee's and customer's first names
+-- and last names ordered by the last name in descending order.
+
---
+++ sqlds/week3/quize7.sql
@@ -0,0 +1,7 @@
+-- All of the questions in this quiz refer to the open source Chinook Database.
+-- Please familiarize yourself with the ER diagram in order to familiarize
+-- yourself with the table and column names in order to write accurate queries
+-- and get the appropriate answers.
+
+-- See if there are any customers who have a different city listed in their
+-- billing city versus their customer city.
